62fecc18296e8adba4d83e1f9ead7e32483d6d50
Farhan Molla <farhanmolla@farhans-air.mynetworksettings.com>
2023-09-18 18:29:02 -0400

    connected backed and frontend

--- flask_react_jobScraper/jobscraper_frontend/src/App.js
+++ flask_react_jobScraper/jobscraper_frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api')
+            .then(response => {
+                setMessage(response.data.message);
+            });
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>{message}</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
