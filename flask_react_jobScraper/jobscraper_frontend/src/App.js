74ff1cd2088216b4176aba1c511710d2f57c58f8
sdurmic <saradurmic@Saras-MacBook-Air-4.local>
2023-10-16 22:38:39 -0400

    login added

--- flask_react_jobScraper/jobscraper_frontend/src/App.js
+++ flask_react_jobScraper/jobscraper_frontend/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route, Switch, BrowserRouter } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import CustomNavbar from './Navbar'; 
 import SearchPage from './SearchBar';
+import Login from './Login';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
@@ -19,15 +21,20 @@ function App() {
     }, []);
 
     return (
+        <Router>
         <div className="App">
             <CustomNavbar/>
             <Container>
                 <header className="App-header">
                  <p>{message}</p>
                 </header>
-                <SearchPage/>
+                <Switch>
+                    <Route path="/login" component={Login} />
+                    <Route exact path="/" component={SearchPage} />
+                </Switch>
             </Container>
         </div>
+        </Router>
     );
 }
 
