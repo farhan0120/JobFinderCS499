f6ca883a16a1db91e0971e94e7b121512c350196
yingliu08 <lying950319@gmail.com>
2023-10-20 19:40:13 -0400

    scam report

--- flask_react_jobScraper/jobscraper_frontend/src/App.js
+++ flask_react_jobScraper/jobscraper_frontend/src/App.js
@@ -1,34 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import CustomNavbar from './Navbar'; 
-import SearchPage from './SearchBar';
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import CustomNavbar from "./Navbar";
+import SearchPage from "./SearchBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-    const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
 
-    useEffect(() => {
-        axios.get('http://127.0.0.1:5000/api')
-            .then(response => {
-                console.log(response.data); 
-                setMessage(response.data.message);
-            });
-    }, []);
+  useEffect(() => {
+    axios.get("http://127.0.0.1:5000/api").then((response) => {
+      console.log(response.data);
+      setMessage(response.data.message);
+    });
+  }, []);
 
-    return (
-        <div className="App">
-            <CustomNavbar/>
-            <Container>
-                <header className="App-header">
-                 <p>{message}</p>
-                </header>
-                <SearchPage/>
-            </Container>
-        </div>
-    );
+  return (
+    <div className="App">
+      <CustomNavbar />
+      <Container>
+        <header className="App-header">
+          <p>{message}</p>
+        </header>
+        <SearchPage />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
